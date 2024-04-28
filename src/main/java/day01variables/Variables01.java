ed61ddea7d1e3e6778f5ff09d27048e0a6d7041f
Husnu Kaya <husnukaya41@gmail.com>
2024-04-28 20:30:48 +0300

    First commit

---
+++ src/main/java/day01variables/Variables01.java
@@ -0,0 +1,45 @@
+package day01variables;
+
+public class Variables01 {// opening class body
+
+    public static void main(String[] args) {
+
+
+        // Data Type   Name of the variable   Assignment Operator value ;
+            int age=15;
+
+
+            boolean isRetired= true;
+            boolean isOld= false;
+
+            char initialOfMyName= 'H';
+        System.out.println(initialOfMyName);
+
+
+        //NON-PRIMITIVE DATA TYPES
+
+        //String: it is a non-primitive data types. It is used to store sequence of characters (e.g texts)
+
+        int shirt= 21;
+
+        double shoes= 12.55, socks=6.99;
+
+        System.out.println(shirt+shoes+socks);
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}//closing class body
